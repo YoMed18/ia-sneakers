041c70ffb983195541ac0c40717d6e6df7c47eaa
lebozec <nicolas.lebozec@xelians.fr>
2024-04-15 15:02:03 +0200

    Modification de la prédiction

--- model/predict.py
+++ model/predict.py
@@ -2,15 +2,14 @@ from ultralytics import YOLO
 from PIL import Image
 import io
 
-model = YOLO('model/runs/detect/train28/weights/best.pt')
+model = YOLO('model/runs/detect/train28/weights/best.pt', 'detect')
 
 
 def predict_image(image_data):
     image = Image.open(io.BytesIO(image_data))
-    model.predict(image)
-    label_name = model.names[0]
-    print(f"Label: {label_name}")
-    return label_name
+    result = model(image)[0]
+    return result.names[0]
+
 
 
 
